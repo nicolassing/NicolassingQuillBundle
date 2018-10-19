4cf4e43d92729f7db2966675a5ac92e621226a12
nicolassing <nicolas.assing@gmail.com>
2018-10-19 18:55:10 +0200

    add bundle class

---
+++ NicolassingQuillBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nicolassing\QuillBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nicolas Assing <nicolas.assing@gmail.com>
+ */
+class NicolassingQuillBundle extends Bundle
+{
+}
