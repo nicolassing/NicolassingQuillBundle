726ffd7bcc1be2fd685f40f21727116c55c37b55
nicolassing <nicolas.assing@gmail.com>
2018-10-22 10:42:19 +0200

    pass options to view

--- Form/Type/QuillType.php
+++ Form/Type/QuillType.php
@@ -4,6 +4,8 @@ namespace Nicolassing\QuillBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -16,6 +18,12 @@ class QuillType extends AbstractType
     public function __construct(array $config)
     {
         $this->config = $config;
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['theme'] = $options['theme'];
+        $view->vars['height'] = $options['height'];
     }
 
     public function configureOptions(OptionsResolver $resolver)
