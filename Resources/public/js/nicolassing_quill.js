2f985dca18e05a5a294cb8e9533b47e9731f5aff
nicolassing <nicolas.assing@gmail.com>
2018-10-22 11:31:41 +0200

    fix javascript

--- Resources/public/js/nicolassing_quill.js
+++ Resources/public/js/nicolassing_quill.js
@@ -1,6 +1,6 @@
 var containers = $(".quill");
 containers.each(function () {
-  var quill = new Quill($(this), {
+  var quill = new Quill($(this).get(0), {
     theme: $(this).data('theme')
   });
   quill.on('text-change', function(delta, oldDelta, source) {
